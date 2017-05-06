aee0bc6921c908f9fd436af1163cd7c9f31c7a74
sfiction <sfiction.r@gmail.com>
2017-05-06 21:44:12 +0800

    main.cc under command line

--- main.cc
+++ main.cc
@@ -56,12 +56,73 @@ void MCTSVsHuman(){
 	printf("%d %d\n", round.getBoard().countBlack(), round.getBoard().countWhite());
 }
 
+void MCTSVsMCTS(){
+	MCTSPlayer a;
+//	ChessBoard board(0x001E0E46426040E1, 0xFE61F1B9BD9FBF1E);
+
+	Round round(a, a);
+	do{
+		round.nextStep();
+		printf("%s\n", round.getBoard().to_string().c_str());
+	}while (!round.isEnd());
+	printf("%d %d\n", round.getBoard().countBlack(), round.getBoard().countWhite());
+}
+
+void MinMaxTest(){
+	MinMaxPlayer a;
+	Round round(a, a, ChessBoard(0x9D52E70E0A142040ull, 0x000C1830752A5FBCull), 0);
+
+//	Round round(a, a, ChessBoard(0x9DC3C3243945657Full, 0x003C3CDBC6BA9A80ull), 1);
+	
+	do{
+		round.nextStep();
+		printf("%s\n", round.getBoard().to_string().c_str());
+	}while (!round.isEnd());
+	printf("%d %d\n", round.getBoard().countBlack(), round.getBoard().countWhite());
+}
+
+void MinMaxVsMCTS(){
+	MCTSPlayer b;
+	MCTSMMPlayer a;
+	Round round(a, b);
+	do{
+		round.nextStep();
+//		printf("\t\tstep %d\n%s\n", round.currentStep(), round.getBoard().to_string().c_str());
+		printf("\t\t\t\t\t\ttime cost = %.3f ms\n", round.getTimeCost());
+	}while (!round.isEnd());
+	printf("%d %d\n", round.getBoard().countBlack(), round.getBoard().countWhite());
+}
+
+//// compete
+void GMMCTSMMVsHuman(){
+	GRANDMCTSMMPlayer a;
+	HumanPlayer b;
+	Round round(a, b);
+
+	double tot[2] = {0.0, 0.0};
+	do{
+		round.nextStep();
+		printf("\t\tstep %d\n%s\n", round.currentStep(), round.getBoard().to_string().c_str());
+		printf("time cost = %.3f ms\n", round.getTimeCost());
+		tot[!round.nextColor()] += round.getTimeCost();
+	}while (!round.isEnd());
+	printf("%d %d\n", round.getBoard().countBlack(), round.getBoard().countWhite());
+	printf("BLACK: %.3f, WHITE: %.3f\n", tot[0], tot[1]);
+}
+
 int main(){
 	srand(time(NULL));
 //	speedTestOfRandomPlayer();
 //	randomVsHuman();
-	MCTSTest();
+//	MCTSTest();
 //	MCTSSingleStepTest();
 //	MCTSVsHuman();
+//	MCTSVsMCTS();
+//	MinMaxTest();
+
+	for (int i = 0; i < 100; ++i)
+		MinMaxVsMCTS();
+	
+//	GMMCTSMMVsHuman();
 	return 0;
 }
